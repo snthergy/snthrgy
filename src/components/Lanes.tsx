760ec14a5af380f6e8a85212957f62b109ae2044
Jon <jon.walz.music@gmail.com>
2021-02-06 23:59:19 -0800

    fix error

--- src/components/Lanes.tsx
+++ src/components/Lanes.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import {Box} from "@material-ui/core";
-import Sound from "./Sound";
+import Sound from "./sound";
 
 const Lanes = (): JSX.Element => {
   return (
