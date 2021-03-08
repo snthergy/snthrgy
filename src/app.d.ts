94f03f95a023f130c142719a492dde601a0e528e
Jon <jon.walz.music@gmail.com>
2021-03-08 10:09:11 -0800

    App state and cleanup

---
+++ src/app.d.ts
@@ -0,0 +1,68 @@
+export interface User {
+  id: string;
+  full_name: string;
+  project: Project;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  tracks: Track[];
+}
+export interface Track {
+  id: string;
+  name: string;
+  osc1: Osc;
+  osc2: Osc;
+  filter: Filter;
+  amp: Amp;
+  reverb: Reverb;
+  delay: Delay;
+}
+
+export interface Osc {
+  id: string;
+  wave: Wave;
+  tune: number | string;
+  pan: number | string;
+}
+
+declare enum Wave {
+  Sin,
+  Square,
+  Rect,
+  Saw,
+}
+
+export interface Filter {
+  id: string;
+  freq: number;
+  reso: number;
+  env: number;
+  attack: number;
+  decay: number;
+  sustain: number;
+  release: number;
+}
+
+export interface Amp {
+  id: string;
+  env: number;
+  attack: number;
+  decay: number;
+  sustain: number;
+  release: number;
+}
+
+export interface Reverb {
+  id: string;
+  mix: number;
+  decay: number;
+}
+
+export interface Delay {
+  id: string;
+  mix: number;
+  length: number;
+  decay: number;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
 import {StylesProvider} from "@material-ui/styles";
 
-import Main from "./pages/Main";
 import Auth0ProviderWithHistory from "./auth/AuthProviderWithHistory";
 import "./styles.css";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => (
   <StylesProvider injectFirst>
     <Router>
       <Auth0ProviderWithHistory>
-        <Main />
+        <Dashboard />
       </Auth0ProviderWithHistory>
     </Router>
   </StylesProvider>
