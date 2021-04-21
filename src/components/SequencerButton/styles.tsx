309790d860015667a1cc8f20beccc4e148309559
Jonathan Freihofer <patientfrei@gmail.com>
2021-04-21 12:09:45 -0400

    slight change in border for UI continuity

--- src/components/SequencerButton/styles.tsx
+++ src/components/SequencerButton/styles.tsx
@@ -5,7 +5,7 @@ export const StyledButton = styled.button`
   color: white;
   background-color: ${({ isActive }) =>
     isActive ? colors.mainBlue : colors.white};
-  border: solid 2.5px ${colors.mainBlue};
+  border: solid 3.5px ${colors.mainBlue};
   width: 60px;
   height: 60px;
   border-radius: 10px;
